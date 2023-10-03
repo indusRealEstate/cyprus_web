caf324293cbf3ae6ef4e1ced2b92739ccae74530
ashiqjak <ashiqflip3@gmail.com>
2023-10-03 18:01:19 +0400

    feat: :zap: 03/10/2023 last

--- src/components/property/property-single-style/single-v4/PropertyHeader.js
+++ src/components/property/property-single-style/single-v4/PropertyHeader.js
@@ -1,31 +1,34 @@
-import listings from "@/data/listings";
-import React from "react";
+import listings from '@/data/listings';
+import React from 'react';
 
 const PropertyHeader = ({ id, data }) => {
-  // const data = listings.filter((elm) => elm.id == id)[0] || listings[0];
-  var formatter = new Intl.NumberFormat(undefined, {
-    style: "currency",
-    currency: "EUR",
-  });
-  return (
-    <>
-      <div className="col-lg-8">
-        <div className="single-property-content mb30-md">
-          <h2 className="sp-lg-title">{`${data.property}`}</h2>
-          <div className="pd-meta mb15 d-md-flex align-items-center">
-            <p className="text fz15 mb-0 bdrr1 pr10 bdrrn-sm">
-              {data.location}
-            </p>
-          </div>
-          <div className="property-meta d-flex align-items-center">
-            <a
-              className="ff-heading text-thm fz15 bdrr1 pr10 bdrrn-sm"
-              href="#"
-            >
-              <i className="fas fa-circle fz10 pe-2" />
-              For {data.unit_purpose == "rent" ? "Rent" : "Sale"}
-            </a>
-            {/* <a
+	// const data = listings.filter((elm) => elm.id == id)[0] || listings[0];
+	var formatter = new Intl.NumberFormat(undefined, {
+		style: 'currency',
+		currency: 'EUR',
+	});
+	return (
+		<>
+			<div className='col-lg-8'>
+				<div className='single-property-content mb30-md'>
+					<h2 className='sp-lg-title'>{`${data.property}`}</h2>
+					<div className='pd-meta mb15 d-md-flex align-items-center'>
+						<p className='text fz15 mb-0 bdrr1 pr10 bdrrn-sm'>
+							{data.location}
+						</p>
+					</div>
+					<div className='property-meta d-flex align-items-center'>
+						<a
+							className='ff-heading text-thm fz15 bdrr1 pr10 bdrrn-sm fw-bolder fs-6'
+							style={{
+								textTransform: 'uppercase',
+                color:"#009f73"
+							}}
+							href='#'>
+							<i className='fas fa-circle pe-2' />
+							For {data.unit_purpose == 'rent' ? 'Rent' : 'Sale'}
+						</a>
+						{/* <a
               className="ff-heading bdrr1 fz15 pr10 ml10 ml0-sm bdrrn-sm"
               href="#"
             >
@@ -37,44 +40,49 @@ const PropertyHeader = ({ id, data }) => {
               <i className="flaticon-fullscreen pe-2 align-text-top" />
               8721
             </a> */}
-          </div>
-        </div>
-      </div>
-      {/* End .col-lg--8 */}
+					</div>
+				</div>
+			</div>
+			{/* End .col-lg--8 */}
 
-      <div className="col-lg-4">
-        <div className="single-property-content">
-          <div className="property-action text-lg-end">
-            <div className="d-flex mb20 mb10-md align-items-center justify-content-lg-end">
-              {/* <a className="icon mr10" href="#">
+			<div className='col-lg-4'>
+				<div className='single-property-content'>
+					<div className='property-action text-lg-end'>
+						<div className='d-flex mb20 mb10-md align-items-center justify-content-lg-end'>
+							{/* <a className="icon mr10" href="#">
                 <span className="flaticon-like" />
               </a> */}
-              <a className="icon mr10" href="#">
-                <span className="flaticon-new-tab" />
-              </a>
-              <a className="icon mr10" href="#">
-                <span className="flaticon-share-1" />
-              </a>
-              <a className="icon" href="#">
-                <span className="flaticon-printer" />
-              </a>
-            </div>
-            {/* <h3 className="price mb-0">{formatter.format(data.price)}</h3> */}
-            <p className="text space fz15">{data.total_area} Sqft</p>
-            <a
-              className="ud-btn btn-thm mb-3"
-              href={`https://premium-realtor.com/api/media/listings/${id}/attachments/${
-                JSON.parse(data.attachments)[0]
-              }`}
-            >
-              DOWNLOAD BROCHURE
-            </a>
-          </div>
-        </div>
-      </div>
-      {/* End .col-lg--4 */}
-    </>
-  );
+							<a
+								className='icon mr10'
+								href='#'>
+								<span className='flaticon-new-tab' />
+							</a>
+							<a
+								className='icon mr10'
+								href='#'>
+								<span className='flaticon-share-1' />
+							</a>
+							<a
+								className='icon'
+								href='#'>
+								<span className='flaticon-printer' />
+							</a>
+						</div>
+						{/* <h3 className="price mb-0">{formatter.format(data.price)}</h3> */}
+						<p className='text space fz15'>{data.total_area} Sqft</p>
+						<a
+							className='ud-btn btn-thm mb-3'
+							href={`https://premium-realtor.com/api/media/listings/${id}/attachments/${
+								JSON.parse(data.attachments)[0]
+							}`}>
+							DOWNLOAD BROCHURE
+						</a>
+					</div>
+				</div>
+			</div>
+			{/* End .col-lg--4 */}
+		</>
+	);
 };
 
 export default PropertyHeader;
