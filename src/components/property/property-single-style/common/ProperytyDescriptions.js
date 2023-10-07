f0338607fe3b56475a63275f57c20b8850ba51cf
indusRealEstate <webtech@indusre.ae>
2023-10-07 15:48:03 +0400

    fix: :bug: fixed showmore text issue.

--- src/components/property/property-single-style/common/ProperytyDescriptions.js
+++ src/components/property/property-single-style/common/ProperytyDescriptions.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 const ProperytyDescriptions = ({ desc }) => {
+  const firstPara = new String(desc)
+    .split(" ")
+    .filter((words, index) => index < 50)
+    .toString()
+    .replace(/,/g, " ");
+
+  const lastPara = new String(desc)
+    .split(" ")
+    .filter((words, index) => index > 50)
+    .toString()
+    .replace(/,/g, " ");
+
   return (
     <>
-      <p className="text mb10">{new String(desc).substring(0, 403)}..</p>
+      <p className="text mb10">{firstPara}..</p>
       <div className="agent-single-accordion">
         <div className="accordion accordion-flush" id="accordionFlushExample">
           <div className="accordion-item">
@@ -15,9 +27,7 @@ const ProperytyDescriptions = ({ desc }) => {
               style={{}}
             >
               <div className="accordion-body p-0">
-                <p className="text">
-                {new String(desc).substring(403)}
-                </p>
+                <p className="text">{lastPara}</p>
               </div>
             </div>
             <h2 className="accordion-header" id="flush-headingOne">
