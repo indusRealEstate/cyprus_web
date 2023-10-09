acea11c54036567236d59251a8d085467d440b29
indusRealEstate <webtech@indusre.ae>
2023-10-09 12:43:42 +0400

    fix: :bug: fixed build time yarn bug after added notfound in property details page.

--- src/app/property-details/index.js
+++ src/app/property-details/index.js
@@ -21,9 +21,10 @@ import PropertyHeader from "@/components/property/property-single-style/single-v
 import PropertyGallery from "@/components/property/property-single-style/single-v4/property-gallery";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
-import { notFound, useSearchParams } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useLayoutEffect, useState } from "react";
-import NotFound from "../[...not_found]/page";
 
 const PropertyDetailsPage = () => {
   const searchParams = useSearchParams();
@@ -42,7 +43,70 @@ const PropertyDetailsPage = () => {
   }, [screenWidth]);
 
   if (data == null) {
-    return <NotFound />;
+    return (
+      <>
+        {/* Main Header Nav */}
+        <DefaultHeader />
+        {/* End Main Header Nav */}
+
+        {/* Mobile Nav  */}
+        <MobileMenu />
+        {/* End Mobile Nav  */}
+
+        {/* Error/404 Section Area */}
+        <section className="our-error">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-xl-6" data-aos="fade-left">
+                <div className="animate_content text-center text-xl-start">
+                  <div className="animate_thumb">
+                    <Image
+                      width={591}
+                      height={452}
+                      className="w-100 h-100 cover"
+                      src="/images/icon/error-page-img.svg"
+                      alt="error-page-img"
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* End .col-6 */}
+
+              <div
+                className="col-xl-5 offset-xl-1 wow fadeInLeft"
+                data-aos="fade-right"
+              >
+                <div className="error_page_content mt80 mt50-lg text-center text-xl-start">
+                  <div className="erro_code">
+                    <span className="text-thm">40</span>4
+                  </div>
+                  <div className="h2 error_title">
+                    Oops! It looks like you&apos;re lost.
+                  </div>
+                  <p className="text fz15 mb20">
+                    The page you&apos;re looking for isn&apos;t available. Try
+                    to search again <br className="d-none d-lg-block" /> or use
+                    the go to.
+                  </p>
+                  <Link href="/" className="ud-btn btn-dark">
+                    Go Back To Homepage
+                    <i className="fal fa-arrow-right-long" />
+                  </Link>
+                </div>
+              </div>
+              {/* End .col-6 */}
+            </div>
+          </div>
+        </section>
+        {/* End Error/404 Section Area */}
+
+        {/* Start Our Footer */}
+        <section className="footer-style1 at-home6 pt60 pb-0">
+          <Footer />
+        </section>
+        {/* End Our Footer */}
+      </>
+    );
   } else {
     return (
       <>
